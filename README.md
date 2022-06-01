4dde39844ea3f99c7dee9a5073ce67fbb5044e2a
Jeremy Ibert <jeremy.ibert@outlook.com>
2022-06-01 12:19:19 +0200

    commit all folders files

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# Trotinettes elec
+
+Site vitrine de Trotinettes electriques en POO. Simple
+
+
+
+
+## Tech Stack
+
+**Client:** Html, Css, Bootstrap
+
+**Server:** Php, Sql
+
+
+## Files
+
+Vous pouvez consulter les documents d'analyse du projet qui sont dans le dossier "analyse"
---
+++ views/commons/template.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="<?=$description?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$title?></title>
+    <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://bootswatch.com/5/spacelab/bootstrap.min.css">
+    <link rel="stylesheet" href="../../public/css/main.css">
+</head>
+<body>
+    <!-- NAV BAR -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img class="img-fluid" width="80" src="../../public/sources/logo/logo.svg" alt="logo"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarColor02">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nos Trotinettes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nous</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- END NAV BAR -->
+
+    <!-- MAIN CONTENT -->
+    <div class="container">
+        <h1 class="text-center text-white bg-secondary border border-light rounded p-1 m-3">Accueil</h1>
+        <?=$content?>
+    </div>
+    <!-- END MAIN CONTENT -->
+
+    <!-- FOOTER -->
+    <footer class="bg-dark text-center text-white rounded-bottom">
+        <p class="p-2">&copy; TrotMan 2022</p>
+    </footer>
+    <!-- END FOOTER -->
+
+    <script src="../../public/js/main.js"></script>
+
+</body>
+</html>
