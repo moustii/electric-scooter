c8af9e2c0bd64b6e38ab5f236a0011923be53e43
Jeremy Ibert <jeremy.ibert@outlook.com>
2022-06-01 12:41:17 +0200

    update: template

--- views/commons/template.php
+++ views/commons/template.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$title?></title>
+    <link rel="icon" href="../../public/sources/logo/logo.png">
     <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://bootswatch.com/5/spacelab/bootstrap.min.css">
@@ -13,13 +14,12 @@
 </head>
 <body>
     <!-- NAV BAR -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary rounded">
         <div class="container-fluid">
             <a class="navbar-brand" href="#"><img class="img-fluid" width="80" src="../../public/sources/logo/logo.svg" alt="logo"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
             <div class="collapse navbar-collapse" id="navbarColor02">
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item">
@@ -39,13 +39,12 @@
 
     <!-- MAIN CONTENT -->
     <div class="container">
-        <h1 class="text-center text-white bg-secondary border border-light rounded p-1 m-3">Accueil</h1>
-        <?=$content?>
+        <h1 class="text-center text-dark bg-light border border-light rounded p-1 m-3">Welcome TrotMan</h1>
     </div>
     <!-- END MAIN CONTENT -->
 
     <!-- FOOTER -->
-    <footer class="bg-dark text-center text-white rounded-bottom">
+    <footer class="bg-primary text-center text-white rounded-bottom">
         <p class="p-2">&copy; TrotMan 2022</p>
     </footer>
     <!-- END FOOTER -->
