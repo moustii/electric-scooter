1db201029ad515ae36fa53536c8c5cb05aaaa1b2
Jeremy Ibert <jeremy.ibert@outlook.com>
2022-06-03 12:27:46 +0200

    add : front controller

---
+++ controllers/front/TrotinettesController.php
@@ -0,0 +1,26 @@
+<?php
+require_once './models/TrotinetteManager.php';
+
+class TrotinettesController {
+    private $trotManager;
+
+    public function __construct() {
+        $this->trotManager = new TrotinetteManager();
+        $this->trotManager->generatorTrotinette();
+    }
+
+    public function getHome() {
+        $trotinettes = $this->trotManager->getTrotinette();
+        require 'views/front/home.view.php';
+    }
+
+    public function getNous() {
+        require 'views/front/nous.view.php';
+    }
+
+    public function displayTrotinettes() {
+        $trotinettes = $this->trotManager->getTrotinettes();
+        require 'views/front/trotinettes.view.php';
+    }
+}
+
